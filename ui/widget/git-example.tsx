c6c71036ac3115b673d86400a14e111be71b5e35
nhatnn <newbie.dev.js@gmail.com>
2023-07-04 22:24:44 +0700

    update

--- ui/widget/git-example.tsx
+++ ui/widget/git-example.tsx
@@ -15,13 +15,10 @@ const logosPartner = [
   '7.png',
   '8.png',
   '9.png',
-<<<<<<< HEAD
-=======
   '10.png',
   '11.png',
   '12.png',
   '13.png',
->>>>>>> 8abf8f8 (update image)
 ];
 
 const Section = () => {
